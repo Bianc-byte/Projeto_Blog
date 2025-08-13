1c813c92898a64fb389d30bab5d5481ffdd9b416
Bianca <biancacunha326@gmail.com>
2025-08-12 23:45:26 -0300

    Criando o model e admin Page (models.Model e admin.ModelAdmin do Django)

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,56 +1,66 @@
 from django.db import models
-from utils.images import resize_image
-from utils.model_validators import validate_png
+from utils.rands import slugify_new
 
 
-class MenuLink(models.Model):
+class Tag(models.Model):
     class Meta:
-        verbose_name = 'Menu Link'
-        verbose_name_plural = 'Menu Links'
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
 
-    text = models.CharField(max_length=50)
-    url_or_path = models.CharField(max_length=2048)
-    new_tab = models.BooleanField(default=False)
-    site_setup = models.ForeignKey(
-        'SiteSetup', on_delete=models.CASCADE, blank=True, null=True,
-        default=None,
-    )
-
-    def __str__(self):
-        return self.text
-
-
-class SiteSetup(models.Model):
-    class Meta:
-        verbose_name = 'Setup'
-        verbose_name_plural = 'Setup'
-
-    title = models.CharField(max_length=65)
-    description = models.CharField(max_length=255)
-
-    show_header = models.BooleanField(default=True)
-    show_search = models.BooleanField(default=True)
-    show_menu = models.BooleanField(default=True)
-    show_description = models.BooleanField(default=True)
-    show_pagination = models.BooleanField(default=True)
-    show_footer = models.BooleanField(default=True)
-
-    favicon = models.ImageField(
-        upload_to='assets/favicon/%Y/%m/',
-        blank=True, default='',
-        validators=[validate_png],
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null=True, blank=True, max_length=255,
     )
 
     def save(self, *args, **kwargs):
-        current_favicon_name = str(self.favicon.name)
-        super().save(*args, **kwargs)
-        favicon_changed = False
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        return super().save(*args, **kwargs)
 
-        if self.favicon:
-            favicon_changed = current_favicon_name != self.favicon.name
+    def __str__(self) -> str:
+        return self.name
 
-        if favicon_changed:
-            resize_image(self.favicon, 32)
 
-    def __str__(self):
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null=True, blank=True, max_length=255,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
         return self.title
